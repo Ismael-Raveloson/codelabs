28a8a2b2ad802c6b2f573964e92ce85177825783
Ismael-Raveloson <isma.raveloson@gmail.com>
2024-08-26 10:28:46 +0300

    lesson 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `<h1>Hello World!</h1>`,
   templateUrl : './app.html',
   styleUrls: ['./app.component.css'],
-  imports : [HomeComponent]
+  imports : [HomeComponent,RouterModule]
 })
 export class AppComponent {
   title = 'homes';
